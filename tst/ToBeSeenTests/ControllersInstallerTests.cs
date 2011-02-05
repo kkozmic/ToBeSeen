a88873244952ca55857e0ff99e16a428f21ab83c
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-02-05 19:22:11 +1000

    - added missing tests, and removed superfluous references

--- tst/ToBeSeenTests/ControllersInstallerTests.cs
+++ tst/ToBeSeenTests/ControllersInstallerTests.cs
@@ -41,6 +41,16 @@
 		}
 
 		[Fact]
+		public void All_controllers_are_named_after_their_type()
+		{
+			var controllersWithWrongName = GetHandlersFor(typeof(IController), containerWithControllers)
+				.Where(controller => controller.ComponentModel.Name != controller.ComponentModel.Implementation.Name)
+				.ToArray();
+
+			Assert.Empty(controllersWithWrongName);
+		}
+
+		[Fact]
 		public void All_controllers_are_registered()
 		{
 			// Is<TType> is an helper, extension method from Windsor
@@ -58,6 +68,16 @@
 				.ToArray();
 
 			Assert.Empty(nonTransientControllers);
+		}
+
+		[Fact]
+		public void All_controllers_expose_themselves_as_service()
+		{
+			var controllersWithWrongName = GetHandlersFor(typeof(IController), containerWithControllers)
+				.Where(controller => controller.Service != controller.ComponentModel.Implementation)
+				.ToArray();
+
+			Assert.Empty(controllersWithWrongName);
 		}
 
 		[Fact]
