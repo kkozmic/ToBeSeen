fc6c0e22cce609770aa06ba72fe60d39bafd4022
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-12-29 11:02:43 +1000

    - updated controllers installer. Also simplified it to just rely on a single rule - IController which is more in line with the approach I've been taking lately

--- src/ToBeSeen/Installers/ControllersInstaller.cs
+++ src/ToBeSeen/Installers/ControllersInstaller.cs
@@ -1,24 +1,18 @@
 using System.Web.Mvc;
+
 using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
-using ToBeSeen.Controllers;
 
 namespace ToBeSeen.Installers
 {
 	public class ControllersInstaller : IWindsorInstaller
 	{
-		#region IWindsorInstaller Members
-
 		public void Install(IWindsorContainer container, IConfigurationStore store)
 		{
-			container.Register(AllTypes.FromThisAssembly()
+			container.Register(Classes.FromThisAssembly()
 			                   	.BasedOn<IController>()
-			                   	.If(Component.IsInSameNamespaceAs<HomeController>())
-			                   	.If(t => t.Name.EndsWith("Controller"))
-			                   	.Configure((c => c.LifeStyle.Transient)));
+			                   	.LifestyleTransient());
 		}
-
-		#endregion
 	}
 }
